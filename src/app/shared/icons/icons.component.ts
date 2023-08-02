ebd02188348bc0af9ba718ee532ffd0d1e65a1ea
Mike Oram <60877515+SlothSan@users.noreply.github.com>
2023-08-02 22:02:32 +0100

    chore: setup icons component for later use.

---
+++ src/app/shared/icons/icons.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dictionary-icons',
+  templateUrl: './icons.component.html',
+  styleUrls: ['./icons.component.scss']
+})
+export class IconsComponent {
+
+}
