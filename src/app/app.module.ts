a5bac42ca01a6f6bb91365947987cf81dd71f462
Mike Oram <60877515+SlothSan@users.noreply.github.com>
2023-08-03 11:48:41 +0100

    feature: have setup the header component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IconsComponent } from './shared/icons/icons.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FontChangerComponent } from './core/components/font-changer/font-changer.component';
+import { ThemeToggleComponent } from './core/components/theme-toggle/theme-toggle.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IconsComponent
+    IconsComponent,
+    HeaderComponent,
+    FontChangerComponent,
+    ThemeToggleComponent
   ],
   imports: [
     BrowserModule,
